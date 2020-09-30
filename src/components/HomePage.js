6c1c80838781dca779887df063ed5d380f86b5c8
leobr <leobrionesherrera@gmail.com>
2020-09-30 14:40:25 -0500

    Completed homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Jumbotron, Button, Container, Row, Col, Card } from 'react-bootstrap'
+import { Jumbotron, Button, Container, Row, Col, Card } from 'react-bootstrap';
+import postJob from '../images/postJob.svg';
+import hire from '../images/hire.svg';
+import complete from '../images/complete.svg';
+import apply from '../images/apply.svg';
+import clean from '../images/clean.svg';
+import completed from '../images/completed.svg';
 
 const HomePage = (props) => {
     return(
@@ -33,43 +39,84 @@ const HomePage = (props) => {
                     </Col>
                 </Row>
                 <br/>
+                <h4 style={{textAlign: 'center'}} >For regular users</h4>
+                <br/>
+                
                 <Row className="row-thumbnails">
+                   
                     <Col>
                         <Card className="homecards" style={{ width: '18rem' }}>
-                            <Card.Img variant="top"  />
+                            <Card.Img variant="top" src={postJob} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Post a job</Card.Title>
                                 <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
+                                Post a job with an address, location, date and time.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
-                    <Card className="homecards" style={{ width: '18rem' }}>
-                        <Card.Img variant="top"  />
+                        <Card className="homecards" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={hire} />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title>Hire someone</Card.Title>
                             <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            Look through all of the people that applied for your job and choose the one that's right for you.
                             </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
                         </Card.Body>
                     </Card>
                     </Col>
                     <Col>
                         <Card className="homecards" style={{ width: '18rem' }}>
-                            <Card.Img variant="top"  />
+                            <Card.Img variant="top" src={complete} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Completed Job</Card.Title>
                                 <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
+                                When the hired person completes the job, mark the job as completed.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <br/>
+                <h4 style={{textAlign: 'center'}} >For cleaners</h4>
+                <br/>
+                <Row className="row-thumbnails">
+                
+                    <Col>
+                        <Card className="homecards" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={apply} />
+                            <Card.Body>
+                                <Card.Title>Apply for a job</Card.Title>
+                                <Card.Text>
+                                Look through all of the posted available jobs and apply to the ones that you'd like to work.
+                                <br/>
+                                <br/>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="homecards" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={clean} />
+                        <Card.Body>
+                            <Card.Title>Clean</Card.Title>
+                            <Card.Text>
+                            When a user hires you, go to the location of the job, at the day and time agreed, and get the job done.
+                            <br/>
+                            <br/>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                        <Card className="homecards" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={completed} />
+                            <Card.Body>
+                                <Card.Title>Completed</Card.Title>
+                                <Card.Text>
+                                Once completed the person that hired you will mark it completed and the job will be added to your completed jobs.
+                                </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
