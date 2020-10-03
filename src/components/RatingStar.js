07ed002f157b110e81a405257096565f99ed2bcb
leobr <leobrionesherrera@gmail.com>
2020-10-03 17:53:19 -0500

    Added compenent for seeing rating

---
+++ src/components/RatingStar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const RatingStar = props => {
+    const displayStars = () => {
+        return [...Array(5)].map((star, i) => {
+            const ratingValue = i + 1;
+            return (
+            <FaStar size={25} 
+                    color={ratingValue <= props.rating ? "#ffc107" : "#e4e5e9"} 
+                    />
+            )
+        });
+    }
+    return displayStars();
+}
+
+export default RatingStar;
